fe3efc22b22e1da38f290c9b57f82f9b80fcb33b
Sergei Nikulov <sergey.nikulov@gmail.com>
2014-03-20 11:56:19 +0400

    fixed include in zmq.cmake

--- FindZMQ.cmake
+++ FindZMQ.cmake
@@ -31,7 +31,7 @@ endif()
 
 # handle the QUIETLY and REQUIRED arguments and set OPENAL_FOUND to TRUE if
 # all listed variables are TRUE
-include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBZMQ DEFAULT_MSG LIBZMQ_LIBRARIES LIBZMQ_INCLUDE_DIR)
 mark_as_advanced(LIBZMQ_LIBRARIES LIBZMQ_INCLUDE_DIR)
 
